8276637f34205eb22d7b9155ed55279b991aadb9
Eeva-Maria Laiho <eeva-maria@laiho.net>
2018-05-28 12:57:12 +0300

    Osa 2, tehtävät 2.7-2.8

---
+++ osa2/kurssit/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Kurssit from './components/Kurssit'
+
+const App = ({ kurssit }) => {
+    return (
+        <Kurssit kurssit={kurssit} />
+    )
+}
+
+export default App
--- osa2/src/App.js
+++ osa2/src/App.js
@@ -1,10 +1,56 @@
-import React from 'react'
-import Kurssit from './components/Kurssit'
+import React from 'react';
 
-const App = ({ kurssit }) => {
-    return (
-        <Kurssit kurssit={kurssit} />
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            persons: [
+                { name: 'Arto Hellas', number: '010 222 3345' },
+                { name: 'Esko Ukkonen', number: '010 123 4567' }
+            ],
+            newName: '',
+            newNumber: ''
+        }
+    }
+    handleNameChange = (event) => {
+        this.setState({ newName: event.target.value })
+    }
+    handleNumberChange = (event) => {
+        this.setState({ newNumber: event.target.value })
+    }
+    addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: this.state.newName,
+            number: this.state.newNumber
+        }
+        const persons = this.state.persons.concat(personObject)
+        this.setState({
+            persons: persons,
+            newName: '',
+            newNumber: ''
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h2>Puhelinluettelo</h2>
+                <form onSubmit={this.addPerson}>
+                    <div>
+                        Nimi: <input value={this.state.newName} onChange={this.handleNameChange}/>
+                    </div>
+                    <div>
+                        Numero: <input value={this.state.newNumber} onChange={this.handleNumberChange}/>
+                    </div>
+                    <div>
+                        <button type="submit">lisää</button>
+                    </div>
+                </form>
+                <h2>Numerot</h2>
+                {this.state.persons.map((person) => <p key={person.name}>{person.name}, {person.number}</p>)}
+            </div>
+        )
+    }
 }
 
 export default App
