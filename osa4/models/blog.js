76d645f62c7d6f8df74494120cc34ab9a2dd4677
Eeva-Maria Laiho <eeva-maria@laiho.net>
2018-06-27 18:52:53 +0300

    4.5

--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -8,6 +8,14 @@ const blogSchema = new Schema({
   likes: Number
 })
 
+blogSchema.statics.format = function(obj) {
+  return {
+    title: obj.title,
+    author: obj.author,
+    likes: obj.likes
+  }
+}
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 module.exports = Blog
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce(function(cnt,b) { return cnt + b.likes }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0)
+    return null
+  const max = blogs.reduce((max, b) => b.likes > max ? b.likes : max, blogs[0].likes)
+  return blogs.filter((blog) => blog.likes === max)[0]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
