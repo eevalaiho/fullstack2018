37f4d80536c266e682d294d738d394655c33aba6
Eeva-Maria Laiho <eeva-maria@laiho.net>
2018-06-28 19:14:03 +0300

    4.12

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,9 +2,46 @@ const blogsRouter = require('express').Router()
 
 const Blog = require('../models/blog')
 
+const formatBlog = (blog) => {
+  return {
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+    id: blog._id
+  }
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog) {
+      response.json(formatBlog(blog))
+    } else {
+      response.status(404).end()
+    }
+
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'malformatted id' })
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+
+    response.status(204).end()
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'malformatted id' })
+  }
 })
 
 blogsRouter.post('/', async (request, response) => {
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -2,145 +2,137 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const { format, initialBlogs, nonExistingId, blogsInDb } = require('./test_helper')
 
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+describe('when there is initially some notes saved', async () => {
 
-beforeAll(async () => {
-  await Blog.remove({})
-  const blogObjs = initialBlogs.map(blog => new Blog(blog))
-  const promiseArray = blogObjs.map(blog => blog.save())
-  await Promise.all(promiseArray)
-})
+  beforeAll(async () => {
+    await Blog.remove({})
+    const blogObjs = initialBlogs.map(blog => new Blog(blog))
+    await Promise.all(blogObjs.map(blog => blog.save()))
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('all blogs are returned as json gy GET /api/blogs', async () => {
+    const blogsInDatabase = await blogsInDb()
 
-test('all blogs are returned', async () => {
-  const response = await api
-    .get('/api/blogs')
-  expect(response.body.length).toBe(initialBlogs.length)
-})
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api
-    .get('/api/blogs')
+    expect(response.body.length).toBe(blogsInDatabase.length)
 
-  const title = response.body.map(r => r.title)
+    const returnedContents = response.body.map(n => n.content)
+    blogsInDatabase.forEach(note => {
+      expect(returnedContents).toContain(note.content)
+    })
+  })
 
-  expect(title).toContain('Canonical string reduction')
-})
+  test('individual blogs are returned as json by GET /api/blogs/:id', async () => {
+    const blogsInDatabase = await blogsInDb()
+    const aBlog = blogsInDatabase[0]
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'React patterns, Part II',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 12
-  }
+    const response = await api
+      .get(`/api/blogs/${aBlog.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    expect(response.body.title).toBe(aBlog.title)
+    expect(response.body.author).toBe(aBlog.author)
+    expect(response.body.url).toBe(aBlog.url)
+  })
 
-  const response = await api
-    .get('/api/blogs')
+  test('404 returned by GET /api/blogs/:id with nonexisting valid id', async () => {
+    const validNonexistingId = await nonExistingId()
 
-  const title = response.body.map(r => r.title)
+    await api
+      .get(`/api/blogs/${validNonexistingId}`)
+      .expect(404)
+  })
 
-  expect(response.body.length).toBe(initialBlogs.length + 1)
-  expect(title).toContain('React patterns, Part II')
-})
+  test('400 is returned by GET /api/blogs/:id with invalid id', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
 
-test('blog without title and url is not added ', async () => {
-  const newBlog = {
-    author: 'New Author',
-    likes: 1
-  }
+    await api
+      .get(`/api/blogs/${invalidId}`)
+      .expect(400)
+  })
 
-  const existingBlogs = await api
-    .get('/api/blogs')
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api
+      .get('/api/blogs')
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    const title = response.body.map(r => r.title)
 
-  const response = await api
-    .get('/api/blogs')
+    expect(title).toContain('Canonical string reduction')
+  })
 
-  expect(response.body.length).toBe(existingBlogs.body.length)
-})
+  describe('addition of a new blog', async () => {
 
-test('blog without likes gets a like value 0', async () => {
-  const newBlog = {
-    title: 'React patterns, Part III',
-    author: 'Michael Chan and Chris Cross',
-    url: 'https://reactpatterns.com/'
-  }
+    test('POST /api/blogs succeeds with valida data', async () => {
+      const blogsBefore = await blogsInDb()
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
+      const newBlog = {
+        title: 'React patterns, Part II',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 12
+      }
 
-  expect(response.body.likes).toBe(0)
-})
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-afterAll(() => {
-  server.close()
+      const response = await api
+        .get('/api/blogs')
+
+      const blogsAfter = await blogsInDb()
+
+      expect(blogsAfter.length).toBe(blogsBefore.length + 1)
+
+      const title = response.body.map(r => r.title)
+
+      expect(title).toContain('React patterns, Part II')
+    })
+
+    test('POST /api/notes fails with proper statuscode if title or url is missing ', async () => {
+      const newBlog = {
+        author: 'New Author',
+        likes: 1
+      }
+
+      const blogsBefore = await blogsInDb()
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAfter = await blogsInDb()
+
+      expect(blogsAfter.length).toBe(blogsBefore.length)
+    })
+
+    test('blog without likes gets a like value 0', async () => {
+      const newBlog = {
+        title: 'React patterns, Part III',
+        author: 'Michael Chan and Chris Cross',
+        url: 'https://reactpatterns.com/'
+      }
+
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+
+      expect(response.body.likes).toBe(0)
+    })
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
 })
