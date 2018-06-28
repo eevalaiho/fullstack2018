e0e1b01d42a90cc7fc64ec6346f1c43463dde9a6
Eeva-Maria Laiho <eeva-maria@laiho.net>
2018-06-28 06:59:37 +0300

    4.6

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,3 +1,5 @@
+
+
 const dummy = (blogs) => {
   return blogs ? 1 : 1
 }
@@ -13,8 +15,25 @@ const favoriteBlog = (blogs) => {
   return blogs.filter((blog) => blog.likes === max)[0]
 }
 
+const mostBlogs = (blogs) => {
+  if (!blogs || blogs.length === 0)
+    return null
+
+  let _ = require('lodash')
+  let topauthor = { 'author':null,'blogs':0 }
+  let author_counts = _.countBy(blogs, blog => blog.author)
+  for (var author in author_counts) {
+    if (author_counts[author] > topauthor['blogs']) {
+      topauthor['author'] = author
+      topauthor['blogs'] = author_counts[author]
+    }
+  }
+  return topauthor
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
